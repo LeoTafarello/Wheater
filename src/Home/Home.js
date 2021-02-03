74e832726c9bddb84a0ad80268aa38f3098f5961
LeoTafarello <leonardo.tafarello@hotmail.com>
2021-02-03 00:00:06 -0300

    Add files via upload

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Home.css';
 
 const api = {
@@ -6,29 +6,50 @@ const api = {
     base:'http://api.openweathermap.org/data/2.5/'
 }
 
-export default class Home extends React.Component{
+export default function Home() {
 
-        state= {
-            loading: true,
+    const [city,Setcity] = useState('')
+    //const [state,Setstate] = useState('')
+    const [weather,SetWheater] = useState({})
+
+        const search = evt => {
+            if (evt.key === 'Enter'){
+                fetch(`${api.base}weather?q=${city}&units=metric&appid=${api.key}`)
+                .then(res => res.json())
+                .then(result => {
+                    SetWheater(result);
+                    Setcity('');
+                })
+            }
         }
 
-        async componentDidMount() {
-            const url = `${api.base}weather?q=London&appid=${api.key}`;
-            const response = await fetch(url);
-            const data = await response.json();
-            console.log(data);
-        }
 
-
-    render(){
         return(
-            <div className='home-main'>
-               <main>
+
+            <div className={(typeof weather.main != 'undefined') ? ((weather.main.temp > 15) ? 'home-main' : 'home-main-cold') : 'home-main'}>
+               <main className='main'>
                    <div className='home-search-row'>
-                       <input className='search-bar' type='text' placeholder='Search...'/>
+                       <input className='search-bar' type='text' placeholder='Search City' onChange= {e=> Setcity(e.target.value)} value={city} onKeyPress={search} />
                    </div>
+                   {(typeof weather.main != 'undefined') ? (
+                   <div>
+                        <div className='location-box'>
+                            <div className='location-name'>{weather.name}, {weather.sys.country}</div>
+                        </div>
+                        <div className='temp-box'>
+                            <div className='temp-celsius'>
+                                {Math.round(weather.main.temp)}º
+                                <span class='after-temp'>C</span>
+                        
+                            </div>
+                            <div className='weather-main'>
+                                <img className='weather-icons' src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt='weather'/>
+                                <div className='weather-name'>{weather.weather[0].main}</div>
+                            </div>
+                        </div>
+                   </div>
+                   ) : ('')}
                </main>
             </div>
         );
-    }
 }
